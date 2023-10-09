d49bb5bc623beef6d3e0e2a27cc86ed481ac9691
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2023-10-09 11:22:39 +0200

    changed namespace of IDatabaseAction to AdminSite.DataAccess.IDatabaseAction

--- AdminSite/DataAccess/IDatabaseAction.cs
+++ AdminSite/DataAccess/IDatabaseAction.cs
@@ -1,13 +1,17 @@
 using AdminSite.Utils;
 using System.Data;
 
-/// <summary>
-/// Defines a contract for classes that represent actions related to the database.
-/// </summary>
-public interface IDatabaseAction
+namespace AdminSite.DataAccess
 {
-    public DataTable Value { get; }
-    public string ConnectionString { get; }
-    public Procedures Procedure { get; }
-    public Dictionary<string, string> Parameters { get; }
+
+    /// <summary>
+    /// Defines a contract for classes that represent actions related to the database.
+    /// </summary>
+    public interface IDatabaseAction
+    {
+        public DataTable Value { get; }
+        public string ConnectionString { get; }
+        public Procedures Procedure { get; }
+        public Dictionary<string, string> Parameters { get; }
+    }
 }
