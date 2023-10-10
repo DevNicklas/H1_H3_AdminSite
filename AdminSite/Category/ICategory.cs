6ec1d245c90b5f084b3c1ab0e36363647a43d29a
rgammelby <rab.gammelby@gmail.com>
2023-10-10 13:34:06 +0200

    icategory interface (wip)

---
+++ AdminSite/Category/ICategory.cs
@@ -0,0 +1,15 @@
+﻿using AdminSite.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminSite.Category
+{
+    public interface ICategory
+    {
+        public int Id { get; }
+        public string Name { get; }
+    }
+}
