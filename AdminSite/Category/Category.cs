7c28dbdc5fa3c32b54b4026120d579d23a79daca
rgammelby <rab.gammelby@gmail.com>
2023-10-10 13:33:55 +0200

    category class (wip)

--- AdminSite/Category/Category.cs
+++ AdminSite/Category/Category.cs
@@ -1,40 +1,109 @@
-﻿using System;
+﻿using AdminSite.DataAccess;
+using AdminSite.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Category
+namespace AdminSite.Category
 {
-    private int _id;
-    private string _name;
-
-    private int SetId()
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Category : ICategory, IStandardActions
     {
+        private int _id;
+        private string _name;
 
-    }
-
-    private string SetName()
-    {
-
-    }
-
-    private DataTable GetAllCategories()
-    {
-        public DataTable GetAllCategories()
+        public Category(DataTable catData)
         {
-            DataTable dt = new DataTable();
-
-            using (SqlConnection conn = GetConnection())
+            try
             {
-                using (SqlCommand cmd = new SqlCommand("SelectAllCategories", conn))
+                if (catData == null)
                 {
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-                    {
-                        sda.Fill(dt);
-                    }
+                    throw new ArgumentNullException();
                 }
-            }
 
-            return dt;
+                _name = catData.Rows[0]["Name"].ToString();
+            }
+            catch
+            {
+                throw;
+            }
         }
+
+        public int Id
+        {
+            get { return _id; }
+            private set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+
+        // RemoveCategory
+        public bool Create(DatabaseAction dbAction)
+        {
+            try
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>
+                {
+                    {"@Name", Name }
+                };
+
+                dbAction.GetData(Procedures.CreateNewCategory, parameters);
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        /* DOESN'T EXIST (SP)
+        public bool Delete(DatabaseAction dbAction)
+        {
+            try
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>
+                {
+                    {"@Name", Name }
+                };
+
+                dbAction.GetData(Procedures.CreateNewCategory, parameters);
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        } */
+        /* DOESN'T EXIST (SP)
+        public bool Update(DatabaseAction dbAction)
+        {
+            try
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>
+                {
+                    {"@Name", Name }
+                };
+
+                dbAction.GetData(Procedures.CreateNewCategory, parameters);
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }*/
+
+        // NewCategory
+
+        // UpdateCategory
     }
+
 }
